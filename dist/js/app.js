d0e8efc35c3ea47633cd4d01c16962049c5c369c
Brenda D. Hart <brendahart1551@gmail.com>
2021-04-27 00:40:42 +0800

    Add smooth scrolling

--- dist/js/app.js
+++ dist/js/app.js
@@ -18123,6 +18123,26 @@ var Scroll = /*#__PURE__*/function () {
     key: "init",
     value: function init() {
       $(window).scroll(function () {// $('nav').toggleClass('active', $(this).scrollTop() > 50);
+      }); // handle links with @href started with '#' only
+
+      $(document).on("click", 'a[href^="#"]', function (e) {
+        // target element id
+        var id = $(this).attr("href"); // target element
+
+        var $id = $(id);
+
+        if ($id.length === 0) {
+          return;
+        } // prevent standard hash navigation (avoid blinking in IE)
+
+
+        e.preventDefault(); // top position relative to the document
+
+        var pos = $id.offset().top; // animated top scrolling
+
+        $("body, html").animate({
+          scrollTop: pos
+        });
       });
     }
   }]);
--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -1,10 +1,31 @@
 /* Set to do semething based on scroll */
 class Scroll {
-    static init() {
-        $(window).scroll(function() {
-            // $('nav').toggleClass('active', $(this).scrollTop() > 50);
-        });
-    }
+  static init() {
+    $(window).scroll(function () {
+      // $('nav').toggleClass('active', $(this).scrollTop() > 50);
+    });
+
+    // handle links with @href started with '#' only
+    $(document).on("click", 'a[href^="#"]', function (e) {
+      // target element id
+      var id = $(this).attr("href");
+
+      // target element
+      var $id = $(id);
+      if ($id.length === 0) {
+        return;
+      }
+
+      // prevent standard hash navigation (avoid blinking in IE)
+      e.preventDefault();
+
+      // top position relative to the document
+      var pos = $id.offset().top;
+
+      // animated top scrolling
+      $("body, html").animate({ scrollTop: pos });
+    });
+  }
 }
 
 export default Scroll;
