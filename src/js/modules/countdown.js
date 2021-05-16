ce3c29156d9de7fa50554c4304725ce210aa5e29
markosandrew <markos.andr1@protonmail.com>
2021-05-17 03:23:27 +0800

    Changed countdown to 03:25 UTC

--- src/js/modules/countdown.js
+++ src/js/modules/countdown.js
@@ -2,7 +2,7 @@
 class Countdown {
   static init() {
     const countdown = () => {
-      const countDate = new Date("May 18, 2021 04:30:11 UTC").getTime();
+      const countDate = new Date("May 18, 2021 03:25:00 UTC").getTime();
       const now = new Date().getTime();
       const gap = countDate - now;
 
