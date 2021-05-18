ca5a01c3c1d37f50933242c968e3bc947d2df27b
markosandrew <markos.andr1@protonmail.com>
2021-05-18 11:29:18 +0800

    Updated timestamp due to block

--- src/js/modules/countdown.js
+++ src/js/modules/countdown.js
@@ -2,7 +2,7 @@
 class Countdown {
   static init() {
     const countdown = () => {
-      const countDate = new Date("May 18, 2021 03:25:00 UTC").getTime();
+      const countDate = new Date("May 18, 2021 03:44:29 UTC").getTime();
       const now = new Date().getTime();
       const gap = countDate - now;
 
